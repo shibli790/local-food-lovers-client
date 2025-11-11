139e795b6cadc8492884bf31bd4479a57ecd4e85
shibli790 <shibliahmed444@gmail.com>
2025-11-11 22:44:02 +0600

    hide firebase api

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,14 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyAypjlLRZJMyb_aATOMrdPfzn5kEHnfZHI',
-//   authDomain: 'fire-base-p-9b66a.firebaseapp.com',
-//   projectId: 'fire-base-p-9b66a',
-//   storageBucket: 'fire-base-p-9b66a.firebasestorage.app',
-//   messagingSenderId: '958792832189',
-//   appId: '1:958792832189:web:467a43c89944251ac153d4',
-// };
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
